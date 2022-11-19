649e6076cc54a2539a4e853aa4b3235ebf7b6364
sumanjangili <118343903+sumanjangili@users.noreply.github.com>
2022-11-19 16:06:04 +0530

    Update Test.java
    one line added  for verify in intellij editor

--- src/main/java/com/example/hellodemo/Test.java
+++ src/main/java/com/example/hellodemo/Test.java
@@ -3,5 +3,6 @@ package com.example.hellodemo;
 public class Test {
     public static void main(String[] args){
         System.out.println("helloooo demoooo");
+        System.out.println("this line of code changes from remote");
     }
 }
